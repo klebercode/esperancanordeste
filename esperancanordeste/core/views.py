3b2c3c3bf4c2c8c2cd06443ab088d3c9949b78c1
Kleber Soares <kleberr@msn.com>
2014-07-01 22:31:33 -0300

    First commit

---
+++ esperancanordeste/core/views.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+from django.shortcuts import render, render_to_response
+from django.views.generic.base import TemplateView
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.db.models import Q
+
+# from forms import * 
+
+def home(request):
+    context = {}
+
+    return render(request, 'index.html', context)
---
+++ esperancanordeste/settings.py
@@ -0,0 +1,103 @@
+# coding: utf-8
+"""
+Django settings for esperancanordeste project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.6/ref/settings/
+"""
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+# import os
+# BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+from decouple import config
+from dj_database_url import parse as db_url
+from unipath import Path
+BASE_DIR = Path(__file__).parent
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = config('SECRET_KEY')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = config('DEBUG', default=False, cast=bool)
+
+TEMPLATE_DEBUG = DEBUG
+
+ALLOWED_HOSTS = ['.localhost', '127.0.0.1', '.herokuapp.com', '.esperancanordeste.com.br', '.ow7.com.br']
+
+
+# Application definition
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    'esperancanordeste.core',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'esperancanordeste.urls'
+
+WSGI_APPLICATION = 'esperancanordeste.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+
+DATABASES = {
+    'default': config(
+        'DATABASE_URL',
+        default='sqlite:///' + BASE_DIR.child('db.sqlite3'),
+        cast=db_url),
+}
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.6/topics/i18n/
+
+LANGUAGE_CODE = 'pt-br'
+
+TIME_ZONE = 'America/Recife'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.6/howto/static-files/
+
+STATIC_ROOT = BASE_DIR.child('staticfiles')
+STATIC_URL = '/static/'
+
+MEDIA_ROOT = BASE_DIR.child('media')
+MEDIA_URL = '/media/'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# DEFAULT_FROM_EMAIL = 'Esperança Nordeste <no-reply@esperancanordeste.com.br>'
+# EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=False, cast=bool)
+# EMAIL_PORT = config('EMAIL_PORT', default=25, cast=int)
+# EMAIL_HOST = config('EMAIL_HOST', default='localhost')
+# EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
+# EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
