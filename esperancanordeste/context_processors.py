57efa12e325808a404352d976760da57d96001b1
Kleber Soares <kleberr@msn.com>
2014-09-25 11:32:48 -0300

    Atualizacao do objeto social no context_process

--- esperancanordeste/context_processors.py
+++ esperancanordeste/context_processors.py
@@ -8,10 +8,11 @@ def enterprise_proc(request):
     """ View Function """
     try:
         enterprise = get_object_or_404(Enterprise, pk=1)
-        social_list = SocialLogo.objects.all()
     except:
         enterprise = ''
-        social_list = ''
+
+    social_list = SocialLogo.objects.all()
+
     return {
         'enterprise': enterprise,
         'social_list': social_list,
@@ -22,10 +23,11 @@ class EnterpriseExtraContext(object):
     """ Class Based View """
     try:
         enterprise = get_object_or_404(Enterprise, pk=1)
-        social_list = SocialLogo.objects.all()
     except:
         enterprise = ''
-        social_list = ''
+
+    social_list = SocialLogo.objects.all()
+
     extra_context = {
         'enterprise': enterprise,
         'social_list': social_list,
