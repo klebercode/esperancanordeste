5357ec2b49cbed270a0e5e47b0e92f4bb61b3c14
Kleber Soares <kleberr@msn.com>
2014-10-02 18:57:15 -0300

    Removendo o return do migration 0001

--- esperancanordeste/sale/migrations/0001_initial.py
+++ esperancanordeste/sale/migrations/0001_initial.py
@@ -8,7 +8,7 @@ from django.db import models
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
-        return
+        # return
         # # Adding model 'Area'
         # db.create_table(u'sale_area', (
         #     (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
